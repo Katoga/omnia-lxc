1be64d2a02162a4b445c3ef33621734284ff48f1
Katoga <katoga.cz@hotmail.com>
2024-07-06 17:32:14 +0200

    Grafana - remove HTTPS
    Self-signed cert causes problems in metrics scraping

--- grafana/usr/sbin/init.sh
+++ grafana/usr/sbin/init.sh
@@ -23,21 +23,7 @@ apt-get update
 apt-get install --assume-yes --no-install-recommends \
   grafana
 
-# enable https with self-signed cert
-cert_path=/etc/grafana/grafana.crt
-cert_key_path=/etc/grafana/grafana.key
-
-openssl genrsa -out "$cert_key_path" 2048
-openssl req -new -key "$cert_key_path" -out /etc/grafana/grafana.csr -subj '/C=CZ/L=Praha/CN=grafana.lan'
-openssl x509 -req -days 365 -in /etc/grafana/grafana.csr -signkey "$cert_key_path" -out "$cert_path"
-chown grafana:grafana "$cert_path"
-chown grafana:grafana "$cert_key_path"
-chmod 400 "$cert_key_path" "$cert_path"
-
-sed -Ei 's~;protocol = http~protocol = https~' /etc/grafana/grafana.ini
-sed -Ei 's~;domain = localhost~domain = grafana.lan~' /etc/grafana/grafana.ini
-sed -Ei "s~;cert_file =~cert_file =${cert_path}~" /etc/grafana/grafana.ini
-sed -Ei "s~;cert_key =~cert_key =${cert_key_path}~" /etc/grafana/grafana.ini
+sed -Ei 's~;(domain =) localhost~\1 grafana.lan~' /etc/grafana/grafana.ini
 
 # disable call-home
 sed -Ei 's~;reporting_enabled = true~reporting_enabled = false~' /etc/grafana/grafana.ini
