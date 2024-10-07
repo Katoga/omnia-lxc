deca403886f3e091292951e54c654f4965402801
Katoga <katoga.cz@hotmail.com>
2024-10-07 21:39:58 +0200

    Librespeed - update Go implementation

--- librespeed/usr/sbin/init.sh
+++ librespeed/usr/sbin/init.sh
@@ -2,28 +2,26 @@
 
 set -euo pipefail
 
+readonly speedtest_cli_version=1.0.11
+readonly librespeed_exporter_version=0.3.2
+
 apt-get update
 apt-get upgrade --assume-yes
 
-apt-get install --assume-yes --no-install-recommends \
-  build-essential \
-  git \
-  golang \
-  prometheus-node-exporter \
-  python3 \
-  python3-prometheus-client
-
 useradd -UM -s /usr/sbin/nologin librespeed-exporter
 
-readonly librespeed_version=1.0.10
-readonly build_script_checksum=821fa881ebbc352ac6808e20462e850a24f3de8df9260976ea8f00de5b46162a475bebcda5d23b20d73f518063fe1bb3d35cf56338503930c52080d8461bb456
-mkdir -p /opt/librespeed
-cd "/opt/librespeed"
-git clone https://github.com/librespeed/speedtest-cli .
-git reset --hard "v${librespeed_version}"
-echo "${build_script_checksum}  build.sh" | sha512sum -c
-./build.sh
-mv ./out/librespeed-cli-* /opt/librespeed-exporter/librespeed-cli
+apt-get install --assume-yes --no-install-recommends \
+  build-essential \
+  ca-certificates \
+  git \
+  golang \
+  prometheus-node-exporter
+
+go install "github.com/librespeed/speedtest-cli@v${speedtest_cli_version}"
+mv ~/go/bin/speedtest-cli /usr/local/bin/
+
+go install "github.com/Katoga/librespeed_exporter@v${librespeed_exporter_version}"
+mv ~/go/bin/librespeed_exporter /usr/local/bin/
 
 mv /opt/librespeed-exporter/librespeed-exporter.service /lib/systemd/system/
 
