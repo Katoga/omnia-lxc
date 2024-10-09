1db1adaa4e31552694883eab45ac4f1202b4c8d7
Katoga <katoga.cz@hotmail.com>
2024-10-09 22:11:02 +0200

    Librespeed - use latest Go for building binaries

--- librespeed/usr/sbin/init.sh
+++ librespeed/usr/sbin/init.sh
@@ -12,10 +12,18 @@ useradd -UM -s /usr/sbin/nologin librespeed-exporter
 
 apt-get install --assume-yes --no-install-recommends \
   build-essential \
+  curl \
   ca-certificates \
   git \
-  golang \
   prometheus-node-exporter
+
+readonly go_version="$(curl -LfSs https://go.dev/VERSION?m=text | head -n1)"
+
+curl -LfSsO "https://go.dev/dl/${go_version}.linux-armv6l.tar.gz"
+rm -rf /usr/local/go && tar -C /usr/local -xzf "${go_version}.linux-armv6l.tar.gz"
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+. ~/.profile
 
 go install "github.com/librespeed/speedtest-cli@v${speedtest_cli_version}"
 mv ~/go/bin/speedtest-cli /usr/local/bin/
