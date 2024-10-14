d4206fb86ec645776d291d7cf2d45acf39ca2ef5
Katoga <katoga.cz@hotmail.com>
2024-10-14 22:07:28 +0200

    Prometheus - fix config

--- prometheus/usr/sbin/init.sh
+++ prometheus/usr/sbin/init.sh
@@ -8,3 +8,7 @@ apt-get upgrade --assume-yes
 apt-get install --assume-yes --no-install-recommends \
   prometheus \
   prometheus-node-exporter
+
+cp -f /opt/prometheus/prometheus.yml /etc/prometheus/prometheus.yml
+
+systemctl reload prometheus
