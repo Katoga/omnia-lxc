1b5349f4d13a8c4bdcd0deb8626bc91248be1f84
Katoga <katoga.cz@hotmail.com>
2024-10-14 22:08:09 +0200

    Prometheus - add Node exporter

--- prometheus/usr/sbin/init.sh
+++ prometheus/usr/sbin/init.sh
@@ -6,7 +6,8 @@ apt-get update
 apt-get upgrade --assume-yes
 
 apt-get install --assume-yes --no-install-recommends \
-  prometheus
+  prometheus \
+  prometheus-node-exporter
 
 cp -f /opt/prometheus/prometheus.yml /etc/prometheus/prometheus.yml
 
