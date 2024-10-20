986e6d719d2d5cacf4630eb530506061bbee80ee
Katoga <katoga.cz@hotmail.com>
2024-10-20 22:35:20 +0200

    Prometheus - enable mDNS via Avahi

--- prometheus/usr/sbin/init.sh
+++ prometheus/usr/sbin/init.sh
@@ -6,8 +6,15 @@ apt-get update
 apt-get upgrade --assume-yes
 
 apt-get install --assume-yes --no-install-recommends \
+  avahi-daemon \
+  avahi-dnsconfd \
+  avahi-utils \
+  libnss-mdns \
   prometheus \
   prometheus-node-exporter
+
+sed -Ei 's~mdns4_minimal~mdns6~' /etc/nsswitch.conf
+sed -Ei 's~(use-ipv4=)yes~\1no~' /etc/avahi/avahi-daemon.conf
 
 cp -f /opt/prometheus/prometheus.yml /etc/prometheus/prometheus.yml
 
