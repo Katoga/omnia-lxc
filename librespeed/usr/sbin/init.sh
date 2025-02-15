af46641dc9a43db30f14555968eda4e897ad0952
Katoga <katoga.cz@hotmail.com>
2025-02-15 12:35:08 +0100

    Librespeed - Install latest Go

--- librespeed/usr/sbin/init.sh
+++ librespeed/usr/sbin/init.sh
@@ -6,7 +6,25 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade --assume-yes
+apt-get install --assume-yes --no-install-recommends \
+  ca-certificates \
+  curl
 
 # enable mDNS
 sed -Ei 's~#?(MulticastDNS=).+$~\1yes~' /etc/systemd/resolved.conf
 sed -Ei 's~(\[Network\])~\1\nMulticastDNS=true~' /etc/systemd/network/eth0.network
+
+# install go
+readonly go_version=1.24.0
+readonly go_checksum=7008bd048a8f82cf12ea44c4ea5dd94437c4248b4ac44315b1ff31b8ced08e4f218d0700d9ce49d8010d5107aa73119d9c1e743f560784fadb9ba0582be342b2
+
+readonly go_tarball="go${go_version}.linux-armv6l.tar.gz"
+curl -LfSsO "https://go.dev/dl/${go_tarball}"
+echo "${go_checksum}  ${go_tarball}" \
+| sha512sum -c --strict
+
+rm -rf /usr/local/go
+tar -C /usr/local -xzf "$go_tarball"
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+. ~/.profile
