644e25864966396202536a93688265c7daca321e
Katoga <katoga.cz@hotmail.com>
2025-02-16 20:47:11 +0100

    Grafana - Install Grafana from their APT repo

--- grafana/usr/sbin/init.sh
+++ grafana/usr/sbin/init.sh
@@ -10,3 +10,21 @@ apt-get upgrade --assume-yes
 # enable mDNS
 sed -Ei 's~#?(MulticastDNS=).+$~\1yes~' /etc/systemd/resolved.conf
 sed -Ei 's~(\[Network\])~\1\nMulticastDNS=true~' /etc/systemd/network/eth0.network
+
+# install Grafana
+apt-get install --assume-yes --no-install-recommends \
+  apt-transport-https \
+  gnupg \
+  software-properties-common \
+  wget
+
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key \
+| gpg --dearmor \
+> /etc/apt/keyrings/grafana.gpg
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" \
+> /etc/apt/sources.list.d/grafana.list
+
+apt-get update
+apt-get install --assume-yes --no-install-recommends \
+  grafana
