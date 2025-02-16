7184bcd30749154017c2eecda8bf952cc88aba0a
Katoga <katoga.cz@hotmail.com>
2025-02-16 21:05:23 +0100

    Grafana - Configure Grafana

--- grafana/usr/sbin/init.sh
+++ grafana/usr/sbin/init.sh
@@ -28,3 +28,13 @@ echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stab
 apt-get update
 apt-get install --assume-yes --no-install-recommends \
   grafana
+
+# configure Grafana
+sed -Ei 's~;?(domain =) localhost~\1 grafana.local~' /etc/grafana/grafana.ini
+
+# disable call-home, see https://grafana.com/docs/grafana/latest/setup-grafana/configure-grafana/#analytics
+sed -Ei 's~;?(reporting_enabled|check_for_updates|check_for_plugin_updates)\s*.+~\1 = false~' /etc/grafana/grafana.ini
+
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl enable grafana-server
