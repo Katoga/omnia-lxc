17244882d39de14e74cd78f5b257c39a1515cc78
Katoga <katoga.cz@hotmail.com>
2026-01-22 18:27:46 +0100

    Librespeed - Update Go

--- librespeed/usr/sbin/init.sh
+++ librespeed/usr/sbin/init.sh
@@ -16,8 +16,8 @@ echo -e '[Resolve]\nMulticastDNS=yes' >> /etc/systemd/resolved.conf.d/mdns.conf
 sed -Ei 's~(\[Network\])~\1\nMulticastDNS=true~' /etc/systemd/network/eth0.network
 
 # install go
-readonly go_version=1.25.4
-readonly go_checksum=119669ed6ca48396d12072651d95fe8d8f79f3f741a9d718a93fe7c9a6dec8c44a3624b5541aa5facb0d1ca7691a879b2d9cf13cf749fa5df2a9a8fdbe524d1d
+readonly go_version=1.25.6
+readonly go_checksum=a5e2040e29cd22222f70a4717c5792440be7c48715049cc6f04c240c62bbaa4e45509db86e7356beedb35b4e2d9000e9d52e2e4cfac69c480dad7c63c3ea548b
 
 readonly go_tarball="go${go_version}.linux-armv6l.tar.gz"
 curl -LfSsO "https://go.dev/dl/${go_tarball}"
