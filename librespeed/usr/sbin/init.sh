0ae8a85414f4b561995131a8a8dfde454e918336
Katoga <katoga.cz@hotmail.com>
2026-06-13 07:58:27 +0200

    Librespeed - Update Exporter

--- librespeed/usr/sbin/init.sh
+++ librespeed/usr/sbin/init.sh
@@ -38,7 +38,7 @@ go install "github.com/librespeed/speedtest-cli@v${speedtest_cli_version}"
 mv ~/go/bin/speedtest-cli /usr/local/bin/
 
 # install librespeed_exporter
-readonly librespeed_exporter_version=0.13.0
+readonly librespeed_exporter_version=0.14.0
 go install "github.com/Katoga/librespeed_exporter@v${librespeed_exporter_version}"
 mv ~/go/bin/librespeed_exporter /usr/local/bin/
 
